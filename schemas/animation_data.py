431bbe2610aa0b0e441c53c9c60195fff5800c7e
Ventura Mendo <ventura.romendo@gmail.com>
2025-03-30 22:05:00 +0100

    remove copy-pasted documentation comment

--- schemas/animation_data.py
+++ schemas/animation_data.py
@@ -2,13 +2,6 @@ from pydantic import BaseModel
 from fastapi import Query
 
 class AnimationData(BaseModel):
-    """
-        - font-family: we should only use monospaced fonts for easiness on positioning (Courier New, Roboto Mono, Consolas, Fira Mono, ...).
-          For proportional fonts, we should use libraries such as PIL (PIL.ImageFont.truetype("myFont.ttf", mySize).getsize(myChar)).
-        - font-size: determines the height of the characters. For monospaced fonts, we can approximate each character's width to 0.6 x font-size.
-        - font-weight: Boldness or thinness: normal (400), bold (700). Varies between 100-900.
-        - font-style: normal|italic|oblique|bold|... not all fonts have all styles.
-    """
     show_time: int
     fade_time: int
     translation_time: int
