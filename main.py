77570bb130300f8c3c3522cecf6c2681a246c88c
Ventura Mendo <ventura.romendo@gmail.com>
2025-03-30 17:12:14 +0100

    svgwrite dummy endpoint tested

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Response
+from pydantic import BaseModel
+import svgwrite
 
 app = FastAPI()
+
+
+class User(BaseModel):
+    name: str
+    age: int
 
 @app.get("/")
 def home():
     return {"message": "SVG TextMorph API is running!"}
+
+@app.get("/generate")
+def generate(
+    words: str = Query(...),
+):
+    # return {"message": f"Words sent are: {words}."}
+    dwg = svgwrite.Drawing(size=("200px", "100px"))
+    dwg.add(dwg.text(words, insert=(10, 20), fill='blue'))
+    svg = dwg.tostring()
+    return Response(content=svg, media_type="image/svg+xml")
